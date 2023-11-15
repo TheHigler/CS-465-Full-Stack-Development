c38fc9b0e31de84051d093e102a7e0cb5d0911ef
TheHigler <rbal88@gmail.com>
2023-11-14 20:18:45 -0500

    Express website, static HTML only

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('./app_server/models/db');
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
-var travelRouter = require('./app_server/routes/travel');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
+const travelRouter = require('./app_server/routes/travel');
 const hbs = require('hbs'); //added to make hbs.registerPartials work
 
 var app = express();
