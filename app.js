005dc31e3ebbba08a9728d7bca3993221b54b201
TheHigler <rbal88@gmail.com>
2023-11-22 10:14:50 -0500

    updates

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-require('./app_server/models/db');
+require('./app_api/database/db');
 
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
 const hbs = require('hbs'); //added to make hbs.registerPartials work
+const apiRouter = require('./app_api/routes/index');
 
 var app = express();
 
@@ -29,6 +30,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
