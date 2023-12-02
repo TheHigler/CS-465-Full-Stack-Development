3e52961915a886acc06372f598e2f173511d0338
TheHigler <rbal88@gmail.com>
2023-12-02 17:21:18 -0500

    update

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'hbs');
-// register handlebars partials (https://www.npmjs.com/package/hbs)
-hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
+    // register handlebars partials (https://www.npmjs.com/package/hbs)
+    hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'));
 
 
 
@@ -26,6 +26,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// allow CORS
+app.use('/api', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELTE');
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
